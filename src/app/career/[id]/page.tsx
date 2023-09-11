78324f60ef297b4c189a3400d89bc496d39703c7
lux <luxofons@gmail.com>
2023-09-11 08:04:32 +0700

    job detail

--- src/app/career/[id]/page.tsx
+++ src/app/career/[id]/page.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
 import { FC } from "react";
 
 type Props = {};
 const JobDetail: FC<Props> = ({}) => {
-  return <></>;
+  return (
+    <div>
+      <div>
+        <span>back</span>
+        <Link href="/career">Back to all positions</Link>
+      </div>
+      <div className="grid grid-cols-3">
+        <div className="col-span-2"></div>
+        <div className="col-span-1">
+          <h3>Apply here</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default JobDetail;
